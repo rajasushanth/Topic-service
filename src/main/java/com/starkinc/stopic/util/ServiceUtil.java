04a122f6e95340d5d550d6e422e5b0d67043eadb
sushneo <sush.neo@gmail.com>
2017-03-12 16:55:48 +0530

    Actuators added and ServiceUtil changes

--- src/main/java/com/starkinc/stopic/util/ServiceUtil.java
+++ src/main/java/com/starkinc/stopic/util/ServiceUtil.java
@@ -3,8 +3,11 @@ package com.starkinc.stopic.util;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-public class ServiceUtil {
+public final class ServiceUtil {
 	public static ResponseEntity<Object> buildEntity(HttpStatus status , Object entity){
 		return new ResponseEntity<Object>(entity, status);
+	}
+	
+	private ServiceUtil(){
 	}
 }
